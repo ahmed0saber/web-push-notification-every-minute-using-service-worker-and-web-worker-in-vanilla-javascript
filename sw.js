b903a1799f169d81432f5715455a4e720d02d68b
Ahmed Saber <ahmed0saber33@gmail.com>
2022-07-02 16:00:22 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,18 @@
-self.addEventListener('install', e => {
-    console.log("Service Worker 1");
-    const title = 'Title';
-    const options = {
-        body: 'Body'
-    };
-    setInterval(() => {
-        self.registration.showNotification(title, options);
-    } ,10000);
-})
+// self.addEventListener('install', e => {
+//     console.log("Service Worker 1");
+//     const title = 'Title';
+//     const options = {
+//         body: 'Body'
+//     };
+//     setInterval(() => {
+//         self.registration.showNotification(title, options);
+//     } ,10000);
+// })
+
+const title = 'Title';
+const options = {
+    body: 'Body'
+};
+setInterval(() => {
+    self.registration.showNotification(title, options);
+} ,10000);
